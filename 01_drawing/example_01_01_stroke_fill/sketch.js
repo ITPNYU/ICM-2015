89b93b066ab6143c83399bdd1ae055aa9775cb11
Daniel Shiffman <daniel@shiffman.net>
2015-09-01 14:21:44 -0400

    week 1 examples from learning processing

---
+++ 01_drawing/example_01_01_stroke_fill/sketch.js
@@ -0,0 +1,13 @@
+// Learning Processing
+// Daniel Shiffman
+// http://www.learningprocessing.com
+
+// Example 1-1: stroke and fill
+
+function setup() {
+  createCanvas(480, 270);
+  background(255);
+  stroke(0); 
+  fill(150);
+  rect(50,50,75,100);
+}
