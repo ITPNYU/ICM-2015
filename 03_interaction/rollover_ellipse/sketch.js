dacab429a8c985d90e83449d60faaca0c9f6ff62
Lauren McCarthy <laurmccarthy@gmail.com>
2015-09-13 21:13:59 -0400

    more gui ex

--- 03_interaction/rollover_ellipse/sketch.js
+++ 03_interaction/rollover_ellipse/sketch.js
@@ -1,19 +1,16 @@
 var x = 300;
 var y = 200;
-var d = 50;
-var g = 0;
+var d = 100;
 
 function setup() {
   createCanvas(600, 400);
 }
 
 function draw() {
-  background(g);
-  ellipse(x, y, d, d);
-}
-
-function mousePressed() {
   if (dist(mouseX, mouseY, x, y) < d/2) {
-    g += 30;
+    background(0);
+  } else {
+    background(255);
   }
+  ellipse(x, y, d, d);
 }
