512ec98da0dc81334db4e50024fddb23421efc91
Lauren McCarthy <laurmccarthy@gmail.com>
2015-09-13 21:05:58 -0400

    adding some mouse ex

--- 03_interaction/if_mouseIsPressed/sketch.js
+++ 03_interaction/if_mouseIsPressed/sketch.js
@@ -1,16 +1,19 @@
+// Getting Started with p5.js
+// Lauren McCarthy, Casey Reas, Ben Fry
+
 function setup() {
-  createCanvas(240, 120);
+  createCanvas(600, 400);
   strokeWeight(30);
 }
 
 function draw() {
   background(204);
   stroke(102);
-  line(40, 0, 70, height);
+  line(140, 0, 170, height);
   if (mouseIsPressed) {
     stroke(0);
   } else {
     stroke(255);
   }
-  line(0, 70, width, 50);
+  line(0, 170, width, 150);
 }
