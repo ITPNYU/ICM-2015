a594de1a22a6da0cef5dd59d94841590f8e3b0f4
Daniel Shiffman <daniel@shiffman.net>
2015-09-29 15:29:23 -0400

    comments and renaming

--- 05_objects_arrays/01_array_syntax/sketch.js
+++ 05_objects_arrays/01_array_syntax/sketch.js
@@ -1,5 +1,7 @@
+// This is declaring an array: a list of 6 values
 var numberlist = [23, 34, 53, -21, 40, 5];
 
+// This is just a single number
 var num = 5;
 
 function setup() {
@@ -10,7 +12,11 @@ function setup() {
 function draw() {
   background(0);
   fill(255);
+  // This is accessing an individual element of the array
+  // by its index (indices start at 0)
   ellipse(100, 100, numberlist[2], numberlist[2]);
+
+  // This is just using an old fashioned variable
   ellipse(200, 100, num, num);
 
 }
--- 05_objects_arrays/03_array_loop1/sketch.js
+++ 05_objects_arrays/03_array_loop1/sketch.js
@@ -10,12 +10,17 @@ function draw() {
   background(0);
   fill(255);
 
+  // Here is how you use a for loop to iterate 
+  // over each element of the array
   for (var i = 0; i < numberlist.length; i++) {
     noFill();
     stroke(255);
     ellipse(i * 100 + 100, 100, numberlist[i], numberlist[i]);
   }
 
+  // The above code is the same as below!
+  // Just uses a loop where i changes dynamically instead
+  // of hard-coded indices
   // ellipse(100, 100, numberlist[0], numberlist[0]);
   // ellipse(200, 100, numberlist[1], numberlist[1]);
   // ellipse(300, 100, numberlist[2], numberlist[2]);
